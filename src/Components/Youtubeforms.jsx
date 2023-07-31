715cfb367044c2b2cbe16a3d9a492bfcea7556c0
Your Name <you@example.com>
2023-07-31 16:51:39 +0600

    start-form-style

---
+++ src/Components/Youtubeforms.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {Stack, Box, Typography} from "@mui/material"
+const Youtubeforms = () => {
+
+    return (
+        <Stack display='flex' justifyContent='center' alignItems='center' margin='2rem 0'>
+            <Typography variant="h3" color='secondary'>React Hook Forms</Typography>
+        </Stack>
+    )
+}
+
+export default Youtubeforms;
