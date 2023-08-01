248c99e5ee591e8a7d0a870f65a90b4d35d21ae8
Your Name <you@example.com>
2023-08-01 09:55:08 +0600

    start-new-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 import { Stack } from "@mui/material";
-import Youtubeforms from "./Components/Youtubeforms";
+// import Youtubeforms from "./Components/Youtubeforms";
+import Reacthookform from "./Components/Reacthookform";
 
 const App = () => {
 
   return (
     <Stack>
-      <Youtubeforms />
+      {/* <Youtubeforms /> */}
+      <Reacthookform />
     </Stack>
   )
 }
---
+++ src/Components/Reacthookform.jsx
@@ -0,0 +1,18 @@
+import { Box, Container, Typography } from "@mui/material";
+
+
+const Reacthookform = () => {
+
+    return (
+        <Container>
+        <Box>
+            <Typography variant="h3" color='primary' textAlign='center' margin='2rem 0' gutterBottom>React form hook</Typography>
+
+
+        </Box>
+        </Container>
+    )
+}
+
+
+export default Reacthookform;
